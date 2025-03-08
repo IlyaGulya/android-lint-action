a21894d87307b0421aa7acf1b963895759559174
Ilya Gulya <ilya@gulya.me>
2025-03-08 12:36:25 +0500

    chore: test

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ import { IOService } from "./effects/actions";
 import { CoreInputs } from "./inputs/core-inputs";
 import { ReviewDog } from "./reviewdog";
 import { XmlConverter } from "./xml-converter";
+import {FileSystem} from "@effect/platform";
 
 const outputs = new CoreOutputs();
 const inputs = new CoreInputs();
@@ -22,27 +23,40 @@ console.log(`CWD:${process.cwd()}`);
 console.log(`Fixtures: ${fs.readdirSync("tests/fixtures").join("\n")}`);
 console.log(`Files: ${fs.readdirSync(".").join("\n")}`);
 
-const program = pipe(
-  runAction(inputs),
-  Effect.provide(
-    Layer.mergeAll(
-      Layer.succeed(ActionOutputs, outputs),
-      IOService.layer,
-      NodeContext.layer,
-      ReviewDog.layer,
-      XmlConverter.layer,
-    ),
+NodeRuntime.runMain(
+  pipe(
+    Effect.gen(function* () {
+      const fs = yield* FileSystem.FileSystem;
+
+      const file = yield* fs.readFileString("tests/fixtures/issues.xml");
+
+      yield* Effect.logInfo(file);
+    }),
+    Effect.provide(NodeContext.layer),
   ),
-  Effect.scoped,
 );
 
-NodeRuntime.runMain(program, {
-  teardown: function teardown(exit, onExit) {
-    if (Exit.isFailure(exit) && !Cause.isInterruptedOnly(exit.cause)) {
-      core.setFailed(exit.cause.toString());
-      onExit(1);
-    } else {
-      onExit(0);
-    }
-  },
-});
+// const program = pipe(
+//   runAction(inputs),
+//   Effect.provide(
+//     Layer.mergeAll(
+//       Layer.succeed(ActionOutputs, outputs),
+//       IOService.layer,
+//       NodeContext.layer,
+//       ReviewDog.layer,
+//       XmlConverter.layer,
+//     ),
+//   ),
+//   Effect.scoped,
+// );
+//
+// NodeRuntime.runMain(program, {
+//   teardown: function teardown(exit, onExit) {
+//     if (Exit.isFailure(exit) && !Cause.isInterruptedOnly(exit.cause)) {
+//       core.setFailed(exit.cause.toString());
+//       onExit(1);
+//     } else {
+//       onExit(0);
+//     }
+//   },
+// });
